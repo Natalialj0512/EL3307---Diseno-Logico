2ccb3ee6ee75c23420fcd10fb165c4f28099e575
Natalialj0512 <natalialj0512@gmail.com>
2026-09-13 16:00:48 -0600

    Cambio binario_7seg: Código más sencillito
    Refactor binary to 7-segment display module with improved signal assignments and segment definitions.

--- EL-3307_template/src/design/binario_7seg.sv
+++ EL-3307_template/src/design/binario_7seg.sv
@@ -1,54 +1,79 @@
 module binario_7seg (
-    input wire [3:0] codigo_bin_pi,
+    input  wire [3:0] codigo_bin_pi,
     output wire [6:0] catodo_po
 );
 
-    wire i3 = codigo_bin_pi[3];
-    wire i2 = codigo_bin_pi[2];
-    wire i1 = codigo_bin_pi[1];
-    wire i0 = codigo_bin_pi[0];
+    wire i3;
+    wire i2;
+    wire i1;
+    wire i0;
 
-    // Segmento a
-    assign catodo_po[2] =
-        (i0 & i1 & i3 & ~i2) |
-        (i0 & ~i1 & ~i2 & ~i3) |
-        (i2 & ~i0 & ~i1 & ~i3);
+    wire a;
+    wire b;
+    wire c;
+    wire d;
+    wire e;
+    wire f;
+    wire g;
 
-    // Segmento b
-    assign catodo_po[3] =
-        (i0 & i1 & i3) |
-        (i1 & i2 & ~i0) |
-        (i2 & i3 & ~i0) |
-        (i0 & i2 & ~i1 & ~i3);
+    assign i3 = codigo_bin_pi[3];
+    assign i2 = codigo_bin_pi[2];
+    assign i1 = codigo_bin_pi[1];
+    assign i0 = codigo_bin_pi[0];
 
-    // Segmento c
-    assign catodo_po[4] =
-        (i1 & i2 & i3) |
-        (i2 & i3 & ~i0);
+    // Segmento A
+    assign a =
+          (i0 & i1 & i3 & ~i2) |
+          (i0 & i2 & i3 & ~i1) |
+          (i0 & ~i1 & ~i2 & ~i3) |
+          (i2 & ~i0 & ~i1 & ~i3);
 
-    // Segmento d
-    assign catodo_po[1] =
-        (i0 & i1 & i2) |
-        (i1 & i3 & ~i0 & ~i2) |
-        (i0 & ~i1 & ~i2 & ~i3) |
-        (i2 & ~i0 & ~i1 & ~i3);
+    // Segmento B
+    assign b =
+          (i0 & i1 & i3) |
+          (i1 & i2 & ~i0) |
+          (i2 & i3 & ~i0) |
+          (i0 & i2 & ~i1 & ~i3);
 
-    // Segmento e
-    assign catodo_po[0] =
-        (i0 & ~i3) |
-        (i0 & ~i1 & ~i2) |
-        (i2 & ~i1 & ~i3);
+    // Segmento C
+    assign c =
+          (i1 & i2 & i3) |
+          (i2 & i3 & ~i0) |
+          (i1 & ~i0 & ~i2 & ~i3);
 
-    // Segmento f
-    assign catodo_po[5] =
-        (i0 & i1 & ~i3) |
-        (i0 & ~i2 & ~i3) |
-        (i1 & ~i2 & ~i3) |
-        (i0 & i2 & i3 & ~i1);
+    // Segmento D
+    assign d =
+          (i0 & i1 & i2) |
+          (i1 & i3 & ~i0 & ~i2) |
+          (i0 & ~i1 & ~i2 & ~i3) |
+          (i2 & ~i0 & ~i1 & ~i3);
 
-    // Segmento g
-    assign catodo_po[6] =
-        (i0 & i1 & i2 & ~i3) |
-        (~i1 & ~i2 & ~i3);
+    // Segmento E
+    assign e =
+          (i0 & ~i3) |
+          (i0 & ~i1 & ~i2) |
+          (i2 & ~i1 & ~i3);
+
+    // Segmento F
+    assign f =
+          (i0 & i1 & ~i3) |
+          (i0 & ~i2 & ~i3) |
+          (i1 & ~i2 & ~i3) |
+          (i0 & i2 & i3 & ~i1);
+
+    // Segmento G
+    assign g =
+          (i0 & i1 & i2 & ~i3) |
+          (~i1 & ~i2 & ~i3) |
+          (i2 & i3 & ~i0 & ~i1);
+
+    // Orden de los cátodos
+    assign catodo_po[6] = g;
+    assign catodo_po[5] = f;
+    assign catodo_po[4] = c;
+    assign catodo_po[3] = b;
+    assign catodo_po[2] = a;
+    assign catodo_po[1] = d;
+    assign catodo_po[0] = e;
 
 endmodule
